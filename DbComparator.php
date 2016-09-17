2ceb48eb66b5349bf553abd3c697ddd6c6c27226
Black Ghoul <royalfadich@gmail.com>
2016-09-17 14:27:55 +0300

    Show tables

--- DbComparator.php
+++ DbComparator.php
@@ -2,21 +2,79 @@
 
 namespace Comparator;
 
+/**
+ * Class DbComparator
+ *
+ * @package     Comparator
+ */
 class DbComparator
 {
-    private $host;
-    private $dbName;
-    private $username;
-    private $password;
+    private $_host;
+    private $_dbName;
+    private $_username;
+    private $_password;
 
-    public function __construct($host = false, $dbName = false, $username = false, $password = null)
+    /** @var  \mysqli $database is mysqli object*/
+    private $_database;
+    private $errors = [];
+
+    /**
+     * DbComparator constructor.
+     *  
+     * @param bool $host
+     * @param bool $username
+     * @param bool $dbName
+     * @param null $password
+     * @throws \Exception
+     */
+    public function __construct($host = false, $username = false, $dbName = false, $password = null)
     {
-        if (!($host && $dbName && $username)) {
+        if (!($host && $username && $dbName)) {
             throw new \Exception('Incorrect connection params.');
         }
+        $this->_host     = $host;
+        $this->_username = $username;
+        $this->_dbName   = $dbName;
+        $this->_password = $password;
+        try {
+            $this->connection();
+        } catch (\Exception $e) {
+            $this->errors[] = $e;
+            throw new \Exception($e->getMessage());
+        }
     }
-    
-    public static function compare(DbComparator $db)
+
+    /**
+     * Connect to database
+     * @throws \Exception
+     */
+    private function connection()
+    {
+        mysqli_report(MYSQLI_REPORT_STRICT);
+        $this->_database = new \mysqli($this->_host, $this->_username, $this->_password, $this->_dbName);
+        if ($this->_database->connect_error) {
+            throw new \Exception('Can not connect to ' . $this->_username . '@' . $this->_host);
+        }
+    }
+
+    public function showTables()
+    {
+        $result = $this->_database->query('SHOW TABLES')->fetch_all();
+        echo '<h3>Tables of ' . $this->_dbName . ':</h3>';
+        foreach ($result as $res) {
+            foreach ($res as $re) {
+                echo $re . '<br>';
+            }
+        }
+    }
+
+    /** @return bool */
+    public function hasErrors()
+    {
+        return !empty($this->errors);
+    }
+
+    public function compare(DbComparator $db)
     {
         
     }
--- index.php
+++ index.php
@@ -2,16 +2,29 @@
 include_once ('DbComparator.php');
 
 use Comparator\DbComparator;
-
-if (empty($_GET))
-{
-    echo "hello comparator";
+/** Hello (head) */
+echo '<h1 align="center">Database comparator</h1>';
+echo '<hr>';
+if (empty($_GET)) {
+    echo '<li>Please, enter params for database connection via GET request.<br>';
+    echo '<li>Set value of any GET-variable as a string having the structure "[host]%%[username]%%[database]%%[password]".<br>';
+} else {
+    foreach ($_GET as $item)
+    {
+        $connection = explode('%%', $item);
+        $databasesCon[] = $connection;
+    }
 }
 
-$params = [];
-
-try{
-    $comparator = new DbComparator($params);
-} catch (\Exception $e) {
-    echo '<br>' . $e->getMessage();
-}
+if (!empty($databasesCon)):
+    foreach ($databasesCon as $databaseCon) {
+        try {
+            $db = new DbComparator($databaseCon[0], $databaseCon[1], $databaseCon[2], $databaseCon[3]);
+            if (!$db->hasErrors()) {
+                $db->showTables();
+            } else { }
+        } catch (\Exception $e) {
+            echo '<br>' . $e->getMessage();
+        }
+    }
+endif;
